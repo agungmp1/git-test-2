a05031e25c7bbc7543deef6c61a3079d1a005792
Agung Maulana <tracemp.amp@gmail.com>
2022-04-04 13:29:19 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel ratione iure sequi quis voluptate quam, nihil perferendis inventore optio autem facere odio debitis expedita velit saepe aspernatur. Quis, aspernatur sit.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
